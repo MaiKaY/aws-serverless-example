9e04464cd29c5cef0bf75afb87e57f9374a28d09
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-06-22 08:49:16 +0200

    add nested routes as an example

--- src/infrastructure/client/api/app.js
+++ src/infrastructure/client/api/app.js
@@ -1,4 +1,3 @@
-import awsServerlessExpressMiddleware from 'aws-serverless-express/middleware';
 import BodyParser from 'body-parser';
 import Express from 'express';
 import morgan from 'morgan';
@@ -8,14 +7,31 @@ const { DEBUG, DEVELOPMENT_MODE } = process.env;
 const app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-app.use(awsServerlessExpressMiddleware.eventContext());
 app.use(morgan('tiny', {
     skip: (req, res) => !(DEBUG || DEVELOPMENT_MODE || res.statusCode >= 400)
 }));
 
 app.get('/', cors(), (req, res) => {
     res.json({
-        hello: 'world'
+        message: 'Hello on /'
+    });
+});
+
+app.get('/foo', cors(), (req, res) => {
+    res.json({
+        message: 'Hello on /foo'
+    });
+});
+
+app.get('/foo/bar', cors(), (req, res) => {
+    res.json({
+        message: 'Hello on /foo/bar'
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Sorry cant find that!'
     });
 });
 

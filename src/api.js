e413932550872237e809c18f227d4b84b10cc6b9
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-08-03 10:49:08 +0200

    change API logic

--- src/api.js
+++ src/api.js
@@ -1,9 +1,20 @@
+import 'dotenv/config';
+import AWS from 'aws-sdk';
 import BodyParser from 'body-parser';
 import Express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import moment from 'moment';
+import uuid from 'uuid/v4';
 
-const { DEBUG, DEVELOPMENT_MODE } = process.env;
+const {
+    BUCKET_NAME,
+    DEBUG,
+    DEVELOPMENT_MODE,
+    IMAGE_TABLE
+} = process.env;
+const s3 = new AWS.S3();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
@@ -11,21 +22,53 @@ app.use(morgan('tiny', {
     skip: (req, res) => !(DEBUG || DEVELOPMENT_MODE || res.statusCode >= 400)
 }));
 
-app.get('/', cors(), (req, res) => {
+app.get('/', cors(), async (req, res) => {
+    const imageId = uuid();
+
+    await dynamoDb.put({
+        TableName: IMAGE_TABLE,
+        Item: {
+            id: imageId,
+            createdAt: moment().utc().toISOString(),
+            status: 'waiting_for_upload'
+        }
+    }).promise();
+
+    const signedUrl = s3.getSignedUrl('putObject', {
+        Bucket: BUCKET_NAME,
+        Key: `temp/${imageId}`,
+        ContentType: 'application/x-www-form-urlencoded',
+        Metadata: {
+            image_id: imageId
+        }
+    });
+
     res.json({
-        message: 'Hello on /'
+        imageId,
+        uploadUrl: signedUrl
     });
 });
 
-app.get('/foo', cors(), (req, res) => {
-    res.json({
-        message: 'Hello on /foo'
-    });
-});
+app.get('/:imageId', cors(), async ({ params: { imageId } }, res) => {
+    const {
+        Item: {
+            createdAt,
+            uploadedAt,
+            status,
+            image
+        }
+    } = await dynamoDb.get({
+        TableName: IMAGE_TABLE,
+        Key: {
+            id: imageId
+        }
+    }).promise();
 
-app.get('/foo/bar', cors(), (req, res) => {
     res.json({
-        message: 'Hello on /foo/bar'
+        createdAt,
+        status,
+        uploadedAt: uploadedAt || '',
+        image: image || ''
     });
 });
 

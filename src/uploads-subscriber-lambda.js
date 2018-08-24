c57e9fcb3a538f85130e66c2cb35f750c51e8274
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-08-24 11:43:34 +0200

    set status to "published"

--- src/uploads-subscriber-lambda.js
+++ src/uploads-subscriber-lambda.js
@@ -1,6 +1,40 @@
 /* eslint-disable import/prefer-default-export */
 
+import AWS from 'aws-sdk';
+import moment from 'moment';
+
+const { IMAGE_TABLE } = process.env;
+const s3 = new AWS.S3();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 export const handler = async (event, context, callback) => {
-    console.log(JSON.stringify(event, null, 4));
+    const originalEvent = JSON.parse(event.Records[0].Sns.Message);
+    const {
+        bucket: { name: bucketName },
+        object: { key: objectKey }
+    } = originalEvent.Records[0].s3;
+
+    const { Metadata } = await s3.getObject({
+        Bucket: bucketName,
+        Key: objectKey
+    }).promise();
+
+    const imageId = Metadata.image_id;
+    const { Item } = await dynamoDb.get({
+        TableName: IMAGE_TABLE,
+        Key: {
+            id: imageId
+        }
+    }).promise();
+
+    await dynamoDb.put({
+        TableName: IMAGE_TABLE,
+        Item: {
+            ...Item,
+            status: 'published',
+            publishedAt: moment().utc().toISOString(),
+            image: objectKey
+        }
+    }).promise();
+
     callback(null, 'Done.');
 };

6a0c1e559ea5817570384bae47e8c27f5ebde8c5
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-08-24 09:43:49 +0200

    update image status

--- src/put-object-subscriber-lambda.js
+++ src/put-object-subscriber-lambda.js
@@ -3,7 +3,9 @@
 import AWS from 'aws-sdk';
 import imageType from 'image-type';
 
+const { IMAGE_TABLE } = process.env;
 const s3 = new AWS.S3();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 export const handler = async (event, context, callback) => {
     const originalEvent = JSON.parse(event.Records[0].Sns.Message);
     const {
@@ -16,15 +18,16 @@ export const handler = async (event, context, callback) => {
         Key: objectKey
     }).promise();
 
-    const { ext, mime } = imageType(Body);
+    const imageId = Metadata.image_id;
+    const { ext, mime } = imageType(Body) || {};
     const validContentTypes = [
         'image/jpg',
         'image/jpeg',
         'image/png'
     ];
 
+    let status;
     if (validContentTypes.some(validContentType => validContentType === mime)) {
-        const imageId = Metadata.image_id;
         await s3.putObject({
             Bucket: bucketName,
             Key: `uploads/${imageId}/${imageId}.${ext}`,
@@ -33,7 +36,25 @@ export const handler = async (event, context, callback) => {
             ContentType: mime,
             Metadata
         }).promise();
+        status = 'waiting_for_thumbnails';
+    } else {
+        status = 'invalid_content_type';
     }
+
+    const { Item } = await dynamoDb.get({
+        TableName: IMAGE_TABLE,
+        Key: {
+            id: imageId
+        }
+    }).promise();
+
+    await dynamoDb.put({
+        TableName: IMAGE_TABLE,
+        Item: {
+            ...Item,
+            status
+        }
+    }).promise();
 
     callback(null, 'Done.');
 };

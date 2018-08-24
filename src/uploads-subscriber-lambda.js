24989c76b86c177c688610f747da3e8330214acb
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-08-24 12:39:59 +0200

    create thumbnails

--- src/uploads-subscriber-lambda.js
+++ src/uploads-subscriber-lambda.js
@@ -2,10 +2,29 @@
 
 import AWS from 'aws-sdk';
 import moment from 'moment';
+import path from 'path';
+import im from 'imagemagick';
 
 const { IMAGE_TABLE } = process.env;
 const s3 = new AWS.S3();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const thumbnails = [
+    {
+        name: 'small',
+        maxHeight: 100,
+        maxWidth: 100
+    },
+    {
+        name: 'medium',
+        maxHeight: 500,
+        maxWidth: 500
+    },
+    {
+        name: 'large',
+        maxHeight: 1000,
+        maxWidth: 1000
+    }
+];
 export const handler = async (event, context, callback) => {
     const originalEvent = JSON.parse(event.Records[0].Sns.Message);
     const {
@@ -13,12 +32,38 @@ export const handler = async (event, context, callback) => {
         object: { key: objectKey }
     } = originalEvent.Records[0].s3;
 
-    const { Metadata } = await s3.getObject({
+    const { Body, ContentType, Metadata } = await s3.getObject({
         Bucket: bucketName,
         Key: objectKey
     }).promise();
 
     const imageId = Metadata.image_id;
+    const extension = path.extname(objectKey);
+
+    await Promise.all(thumbnails.map(thumbnail => (
+        new Promise((resolve, reject) => {
+            im.resize({
+                srcData: Body,
+                height: thumbnail.maxHeight,
+                width: thumbnail.maxWidth
+            }, (resizeError, resizeData) => {
+                if (resizeError) {
+                    reject(resizeError);
+                }
+                s3.putObject({
+                    Bucket: bucketName,
+                    Key: `thumbnails/${imageId}/${thumbnail.name}.${extension}`,
+                    Body: Buffer.from(resizeData, 'binary'),
+                    ACL: 'public-read',
+                    ContentType,
+                    Metadata
+                }, (error, data) => (
+                    error ? reject(error) : resolve(data)
+                ));
+            });
+        })
+    )));
+
     const { Item } = await dynamoDb.get({
         TableName: IMAGE_TABLE,
         Key: {
